b249c1f802eb9b0fabe0acdf1ed0ec8e4cda8144
Özkan Büyük <ozkanbuyuk@icloud.com>
2024-02-26 01:23:08 +0300

    Next.js - JWT Auth

---
+++ src/app/login/page.jsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function LoginPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const username = formData.get("username");
+    const password = formData.get("password");
+
+    const res = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+    });
+    const { success } = await res.json();
+
+    if (success) {
+      const nextUrl = searchParams.get("next");
+      router.push(nextUrl ?? "/");
+      router.refresh();
+    } else {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
